f89d8f230d3925e8624eeafcb6d338c45feb50c9
Emanuel Bragança <emgbraganca@gmail.com>
2022-01-22 14:24:43 -0300

    first commit

---
+++ banco.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `cadastro`.`usuarios` ( `id_cliente` INT NOT NULL AUTO_INCREMENT , 
+`nome_cliente` VARCHAR(255) NOT NULL , 
+`email_cliente` VARCHAR(255) NOT NULL , 
+`telefone_cliente` INT(14) NOT NULL , 
+`data_nasc_cliente` DATE NOT NULL , 
+`senha_cliente` VARCHAR(255) NOT NULL , 
+PRIMARY KEY (`id_cliente`)) ENGINE = InnoDB;
